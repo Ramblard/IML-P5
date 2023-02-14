a742e9af3be3a8742666e09a25d1ecf2abefb741
Ramblard <amblard.remi@gmail.com>
2023-02-14 12:17:35 +0100

    app.py

--- API/app.py
+++ API/app.py
@@ -1,6 +1,6 @@
 
 import flask
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import sys
 sys.path.append('./scripts/')
 from predictions import *
@@ -8,20 +8,23 @@ from predictions import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return flask.render_template('index.html')
+    if request.method == 'POST':
+        text = request.form['text']
+        return redirect('/tags?text=' + text)
+    return render_template('index.html')
 
 
 @app.route('/tags', methods = ['GET'])
 def tags():
 
-    question = request.args.get('question')
-    predicted_tags = predire(question)
+    text = request.args.get('text')
+    predicted_tags = predire(text)
     nombre_tags = len(predicted_tags)
     predicted_tags = ' '.join(predicted_tags)
 
-    return  jsonify({"question":question, "nombre_tags":nombre_tags, "tags":predicted_tags})
+    return  jsonify({"question":text, "nombre_tags":nombre_tags, "tags":predicted_tags})
 
 
 if __name__ == "__main__":
